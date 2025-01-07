3c3ed09eaef12b7a2bd139de0baa805b92b71159
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-01-08 02:28:22 +0700

    Simple components making

--- project1-reactjs/src/App.jsx
+++ project1-reactjs/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = "This is the title";
+  const gglink = "http://www.google.com";
 
   return (
     <>
+      <Navbar/>
+      <Home/>
 
-      <div className="app">
-        <div className="content">
-          <h1>App Component</h1>
-        </div>
+      <div className="content">
+        <h3>App Component</h3>
+        <h4>{ title }</h4>
+        <h4>{ "another title" }</h4>
+        <p>{ ["This","is","an","array",":",1,2,3,4,5,6,7,8,9] }</p>
+        <p>{ Math.random() }</p>
+
+        <a href={ gglink }>Google Link</a>
       </div>
 
     </>
---
+++ project1-reactjs/src/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h3>Project 1</h3>
+            <div className="navlinks">
+                <a href="/">Home</a>
+                <a href="">New post</a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
